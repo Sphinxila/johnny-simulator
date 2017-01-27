6b27805847bfa701120cec5b4e08bbe3f586892a
Sphinx <sphinx@sphinx.li>
2017-01-27 07:00:07 +0100

    added johnny source (lazarus project)

---
+++ src/johnny/Programm/cleanup.bat
@@ -0,0 +1,10 @@
+Rem Batch-Datei zum Löschen überflüssiger Dateien
+Rem =============================================
+
+del *.o
+del *.ppu
+del *.compiled
+del *.exe
+del *.or
+del *.res
+rmdir backup /Q
